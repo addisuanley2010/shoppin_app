00cdb27d573d555cb01357745a4edee9d927358d
addisuanley2010 <addisuanley2005@gmail.com>
2022-12-20 14:12:23 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
+import Home from './components/Home';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
